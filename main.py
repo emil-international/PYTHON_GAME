148d7baa9d0c9db718fb6d2cea31ddcfb4e740d2
emil-international <58439947+emil-international@users.noreply.github.com>
2020-04-20 14:37:48 +0300

    исправили пару проблем

--- main.py
+++ main.py
@@ -36,10 +36,9 @@ class GameObject:
 
     def draw(self):
         self.picture.blit(self.x, self.y)
-
-
 class Unit(GameObject):
-    def set_collision(self, x_right_velocity=-1, x_left_velocity=-1, y_up_velocity=-1, y_down_velocity=-1):
+    def set_collision(self, x_right_velocity=-1, x_left_velocity=-1,
+                      y_up_velocity=-1, y_down_velocity=-1):
         if (x_right_velocity >= 0) and (self.vx >= 0):
             self.vx = x_right_velocity
         if (x_left_velocity >= 0) and (self.vx <= 0):
@@ -54,8 +53,6 @@ class Unit(GameObject):
             self.ax = 100;
         if (self.vx > 10):
             self.ax = -100
-
-
 class Zombie_usual(Unit):
     def __init__(self, x, y, res, hero):
         super().__init__(x, y, res)
@@ -83,8 +80,6 @@ class Zombie_usual(Unit):
                 else:
                     self.vx = 40 * randint(1, 10)
                     self.ax = 0
-
-
 class Zombie_Boss(Unit):
     def __init__(self, x, y, res, hero):
         super().__init__(self, x, y, res)
@@ -104,8 +99,6 @@ class Zombie_Boss(Unit):
 
         elif (self.hero.y >= self.x) and (self.ay < 30):
             self.ay += 2
-
-
 class Hero(Unit):
     def __init__(self, x, y, res):
         super().__init__(x, y, res)
@@ -130,8 +123,6 @@ class Hero(Unit):
     def jump(self):
         if ((self.y == 0) or (self.concerns == True)):
             self.vy = self.jump_speed
-
-
 class wall(GameObject):
     def __init__(self, x, y, res, orientation, length):
         super().__init__(x, y, res)
@@ -179,17 +170,40 @@ class GameWindow(pyglet.window.Window):
         self.shoot = 0
 
         self.hero = Hero(10, 100, resourses())
-        self.zombies = [Zombie_usual(randint(200, 300), randint(600, 800), resourses(), self.hero) for i in range(100)]
-
+        self.zombies = [Zombie_usual(randint(200, 300),
+                                     randint(600, 800),
+                                     resourses(),
+                                     self.hero) for i in range(3)]
         self.walls = []
+        self.walls.append(wall(10,
+                               50,
+                               resourses(),
+                               "horiz",
+                               780))
 
-        self.walls.append(wall(10, 50, resourses(), "horiz", 780))
-        self.walls.append(wall(300, 250, resourses(), "horiz", 200))
+        self.walls.append(wall(300,
+                               250,
+                               resourses(),
+                               "horiz",
+                               200))
 
-        self.walls.append(wall(10, 150, resourses(), "horiz", 100))
-        self.walls.append(wall(690, 150, resourses(), "horiz", 100))
+        self.walls.append(wall(10,
+                               150,
+                               resourses(),
+                               "horiz",
+                               100))
 
-        self.walls.append(wall(10, 50, resourses(), "vert", 600))
+        self.walls.append(wall(690,
+                               150,
+                               resourses(),
+                               "horiz",
+                               100))
+
+        self.walls.append(wall(10,
+                               50,
+                               resourses(),
+                               "vert",
+                               600))
         self.walls.append(wall(790, 50, resourses(), "vert", 600))
 
         self.bullets = []
@@ -198,40 +212,7 @@ class GameWindow(pyglet.window.Window):
         self.clear()
         self.hero.draw()
 
-        text = "LOL NICE!"
-
-
-
-        if(self.hero.hp < 0):
-            if abs(self.hero.hp) < self.hero.points:
-                text = "LOL U'RE NOT NOOB"
-
-            elif abs(self.hero.hp) >= self.hero.points:
-                text = "LOL U'RE NOOB"
-
-            elif abs(self.hero.hp) > self.hero.points * 10:
-                text = "LOL U'RE pro."
-
-        print(len(self.bullets))
-
-        label = pyglet.text.Label('hp ' + str(self.hero.hp),
-                                  font_name='Times New Roman',
-                                  font_size=36,
-                                  x=10, y=10)
-
-        label2 = pyglet.text.Label('points ' + str(self.hero.points),
-                                   font_name='Times New Roman',
-                                   font_size=36,
-                                   x=600, y=10)
-
-        label3 = pyglet.text.Label(text,
-                                   font_name='Times New Roman',
-                                   font_size=26,
-                                   x=200, y=10)
-
-        label.draw()
-        label2.draw()
-        label3.draw()
+        self.draw_interface()
 
         for z in self.zombies:
             z.draw()
@@ -240,7 +221,6 @@ class GameWindow(pyglet.window.Window):
 
         for bul in self.bullets:
             bul.draw()
-
     def collision_walls(self, dt, object1):
         for wall in self.walls:
             if (wall.orientation == "horiz"):
@@ -255,11 +235,6 @@ class GameWindow(pyglet.window.Window):
                     object1.x + object1.picture.width - abs(
                         object1.vx) * dt) <= wall.x + wall.length))):  # удар башкой
                     object1.set_collision(-1, -1, 0, -1)
-                # if ((self.hero.y + self.hero.vy * dt < wall.y) and (self.hero.y + self.hero.picture.height + self.hero.vy * dt > wall.y) and (self.hero.x + self.hero.picture.width == wall.x)):
-                # Hero.set_collision(self.hero, 0, -1, -1, -1)
-                # if ((self.hero.y < wall.y) and (self.hero.y + self.hero.picture.height > wall.y) and
-                # (self.hero.x == wall.x + wall.length)):
-                # Hero.set_collision(self.hero, -1, 0, -1, -1)
             else:
                 if ((abs(object1.x - wall.x) <= abs(object1.vx) * dt) and (
                         (wall.y <= abs(object1.y) <= wall.y + wall.length) or (
@@ -273,28 +248,6 @@ class GameWindow(pyglet.window.Window):
                     object1.concerns = True
                     object1.x = wall.x - object1.picture.width - 1
                     object1.set_collision(0, -1, -1, -1)
-
-            # wall.y <= abs(self.hero.y + abs(self.hero.vy) * dt) <= wall.y + 100
-
-            # for zombie in self.zombies:
-            # if (wall.orientation == "horiz"):
-            # if ((abs(zombie.y - wall.y) <= abs(zombie.vy) * dt) and ((wall.x <= abs(zombie.x - abs(zombie.vx) * dt) <= wall.x + wall.length) or (wall.x <= abs(zombie.x + zombie.picture.width - abs(zombie.vx) * dt) <= wall.x + wall.length))):
-            # self.zombie.concerns = True
-            # Zombie_usual.set_collision(zombie, -1, -1, 0, 0)
-            # if  ((abs(zombie.y + zombie.picture.height - wall.y) <= abs(zombie.vy) * dt) and ((wall.x <= abs(zombie.x - abs(zombie.vx) * dt) <= wall.x + wall.length) or (wall.x <= abs(zombie.x + zombie.picture.width - abs(zombie.vx) * dt) <= wall.x + wall.length))):  #удар башкой
-            # Zombie_usual.set_collision(zombie, -1, -1, 0, -1)
-            # if ((zombie.y < wall.y) and (zombie.y + zombie.picture.height > wall.y) and (zombie.x + zombie.picture.width == wall.x)):
-            # Zombie_usual.set_collision(zombie, 0, -1, -1, -1)
-            # if ((zombie.y < wall.y) and (zombie.y + zombie.picture.height > wall.y) and
-            # (zombie.x == wall.x)):
-            # Zombie_usual.set_collision(zombie, -1, 0, -1, -1)
-            # else:
-            # if ((abs(zombie.x - wall.x) <= abs(zombie.vx) * dt) and ((wall.y <= abs(zombie.y) <= wall.y + wall.length) or (wall.y <= abs(zombie.y + zombie.picture.height) <= wall.y + wall.length))): # стена слева
-            # self.hero.concerns = True
-            # Zombie_usual.set_collision(zombie, -1, 0, -1, -1)
-            # elif ((abs(wall.x - zombie.x - zombie.picture.width - 1) <= abs(zombie.vx) * dt) and ((wall.y <= abs(zombie.y) <= wall.y + wall.length) or (wall.y <= abs(zombie.y + zombie.picture.height) <= wall.y + wall.length))): # стена справа
-            # self.hero.concerns = True
-            # Zombie_usual.set_collision(zombie, 0, -1, -1, -1)
 
     def collision_objects(self, dt, object1, object2):
         if ((object2.x <= object1.x + object1.picture.width <= object2.x + object2.picture.width) and (
@@ -327,14 +280,17 @@ class GameWindow(pyglet.window.Window):
 
         if symbol == key.DOWN:
             self.shoot = 1
-
     def update(self, dt):
         self.hero.update_positions(dt)
 
-        for i in range(len(self.bullets)):
+        i = len(self.bullets) - 1
+        while i >= 0:
             if (self.bullets[i].x >= 780) or (self.bullets[i].x <= 10):
+                self.bullets[i].dead = True
+            if (self.bullets[i].dead == True):
+                self.bullets[i].y -= 1000
                 del self.bullets[i]
-                break
+            i -= 1
 
         if self.shoot == 1:
             if self.hero.orientation == 1:
@@ -368,6 +324,39 @@ class GameWindow(pyglet.window.Window):
         for zombie in self.zombies:
             self.collision_walls(dt, zombie)
 
+    def draw_interface(self):
+        text = "LOL NICE!"
+
+        if (self.hero.hp < 0):
+            if abs(self.hero.hp) < self.hero.points:
+                text = "LOL U'RE NOT NOOB"
+
+            elif abs(self.hero.hp) >= self.hero.points:
+                text = "LOL U'RE NOOB"
+
+            elif abs(self.hero.hp) > self.hero.points * 10:
+                text = "LOL U'RE pro."
+
+        print(len(self.bullets))
+
+        label = pyglet.text.Label('hp ' + str(self.hero.hp),
+                                  font_name='Times New Roman',
+                                  font_size=36,
+                                  x=10, y=10)
+
+        label2 = pyglet.text.Label('points ' + str(self.hero.points),
+                                   font_name='Times New Roman',
+                                   font_size=36,
+                                   x=600, y=10)
+
+        label3 = pyglet.text.Label(text,
+                                   font_name='Times New Roman',
+                                   font_size=26,
+                                   x=200, y=10)
+
+        label.draw()
+        label2.draw()
+        label3.draw()
 
 if __name__ == "__main__":
     window = GameWindow(800, 600)
